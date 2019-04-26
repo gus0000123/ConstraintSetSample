05da7fbc6b9918fe7213e845835e284cbf5d1815
LeeJongHyun <leejh8848@hsociety.co.kr>
2019-04-26 17:04:34 +0900

    Sample maked

--- app/src/main/java/com/hyun/android/constraintsetsample/MainActivity.kt
+++ app/src/main/java/com/hyun/android/constraintsetsample/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.hyun.android.constraintsetsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.util.Log
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.BounceInterpolator
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isopen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_add.setOnClickListener {
+            if (isopen) {
+                Log.d("TAG","btn_add onClick")
+                updateView(R.layout.activity_main_alt)
+                btn_add.text = "+"
+                tv_contents.text = "2019-04-26 15:57 합정동에서 작성"
+
+            } else {
+                Log.d("TAG","btn_add_back onClick")
+                updateView(R.layout.activity_main)
+                btn_add.text = "+ 할일 추가"
+                tv_contents.text = "할일 목록을 입력하세요"
+            }
+            isopen = !isopen
+        }
+    }
+
+    fun updateView(@LayoutRes id: Int) {
+        var targetConstSet = ConstraintSet()
+        targetConstSet.clone(this, id)
+        targetConstSet.applyTo(root)
+
+        val trans = ChangeBounds()
+        trans.interpolator = AccelerateInterpolator()
+        TransitionManager.beginDelayedTransition(root, trans)
     }
 }
